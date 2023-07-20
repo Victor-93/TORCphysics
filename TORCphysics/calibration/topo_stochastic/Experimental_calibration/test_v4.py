da1f709fc0495d11cefdf88bc91c21cd17df8a9a
Victor <V.VelascoBerrelleza@leeds.ac.uk>
2023-07-20 18:08:13 +0100

    I am calibrating the topoisomerase activity according experimental assays

--- TORCphysics/calibration/topo_stochastic/Experimental_calibration/test_v4.py
+++ TORCphysics/calibration/topo_stochastic/Experimental_calibration/test_v4.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from TORCphysics import Circuit
 
 # ----------------------------------------------------------------------------------------------------------------------
 # DESCRIPTION
@@ -7,32 +8,54 @@ import numpy as np
 # This is just a test to reproduce the global supercoiling response curves from the paper:
 # Kinetic Study of DNA Topoisomerases by Supercoiling-Dependent Fluorescence Quenching
 
+# We now have estimations of the initial and final superhelical densities.
+# For now, let's not consider ATP.
+# Let's run examples of the actual calibration. Remember that in your simulation, you need to consider the
+# density of the plasmid concentration as well, so you have many concentrations to test.
+
 # TODO:
-#  1.- Do it for gyrase response.
-#  2.- See how you set the initial supercoiling and final (probably it does not tend to 0).
-#  3.- See how you can add the kinetics with ATP to your model.
-#  4.- And check weather you need to update the shape of your recognition curves (probably not, or hopefully).
-#  5.- And finally, check if from the experimental K_M, you can propose your k_on and k_off.
+#  1.- Run parallelization test, varying plasmid concentrations.
+#  2.- MY initial thought was that according K_M & k_cat, I could determine k_on and k_off, but probably
+#  I'll need to determine these parameters. I think I can consider a rapid disassociation, and slow binding.
+#  3.- So in general, the parameters I need to optimize are k_on, k_off, k_cat, width and threshold.
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Initial conditions
 # ----------------------------------------------------------------------------------------------------------------------
 # Units:
 # concentrations (nM), K_M (nM), velocities (nM/s), time (s)
-initial_substrate = .35
-initial_substrates = [2.1]  # [2.1, 1.8, 1.1, .7, .35]
 dt = 1
 initial_time = 0
-final_time = 400
-initial_sigma = -.04
-final_sigma = 0.0
+final_time = 600
 time = np.arange(initial_time, final_time + dt, dt)
 frames = len(time)
+
+# For the simulation
+circuit_filename = 'circuit_test.csv'
+sites_filename = 'sites_test.csv'
+enzymes_filename = 'enzymes_test.csv'
+environment_filename = 'environment_test.csv'
+tm = 'continuum'
+output_prefix = 'test0'
+series = True
+continuation = False
+mm = 'uniform'
+
+# Meyer topos curve parameters:
+topo_w = 0.012  # width
+topo_t = -0.04  # thresholds
+topo_k = 0.001  # k_cat
+gyra_w = 0.025  # width
+gyra_t = 0.01  # threshold
+gyra_k = 0.001  # k_cat
 
 # Figure
 width = 6
 height = 3
-fig, axs = plt.subplots(2, 2, figsize=(1.25 * width, 2 * height), tight_layout=True)
+lw = 3
+colors = ['blue', 'red', 'green', 'black', 'grey', 'yellow', 'cyan', 'purple', 'brown', 'pink']
+fig, axs = plt.subplots(2, 2, figsize=(2 * width, 2 * height), tight_layout=True)
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -86,54 +109,63 @@ def rescale_product_to_sigma(product, sigma_min, sigma_max):
 # Kinetics: SDNA + TopoI -> SDNA-TopoI -> RDNA + TopoI
 # Product = Fluorescent or Relaxed DNA
 # Substrate = Concentration of Supercoiled DNAs
+initial_sigma = -.047
+final_sigma = 0.0
 initial_product = 0.0
 initial_substrate = .7
+initial_substrates = [0.35, 0.7, 1.1, 1.8, 2.1]
 enzyme_concentration = 17
 K_M = 1.5
 k_cat = .0023  # 0.003
 v_max = k_cat * enzyme_concentration
-# Substrates and products
-# ----------------------------------
-ax = axs[0, 0]
-substrate, product = integrate_MM(vmax=v_max, KM=K_M, substrate0=initial_substrate, product0=initial_product,
-                                  frames=frames, dt=dt)
-ax.plot(time, product)
+for count, initial_substrate in enumerate(initial_substrates):
+    # Substrates and products
+    # ----------------------------------
+    ax = axs[0, 0]
+    substrate, product = integrate_MM(vmax=v_max, KM=K_M, substrate0=initial_substrate, product0=initial_product,
+                                      frames=frames, dt=dt)
+    ax.plot(time, product, lw=lw, color=colors[count])
 
-# Sigma deduction
-# ----------------------------------
-ax = axs[1, 0]
-superhelical = rescale_product_to_sigma(product, initial_sigma, final_sigma)
-ax.plot(time, superhelical)
+    # Sigma deduction
+    # ----------------------------------
+    ax = axs[1, 0]
+    superhelical = rescale_product_to_sigma(product, initial_sigma, final_sigma)
+    ax.plot(time, superhelical, lw=lw, color=colors[count])
 
 # Gyrase
 # ==================================================================================================================
 # Kinetics: RDNA + Gyrase + ATP -> RDNA-Gyrase-ATP -> SDNA + Gyrase + ADP
 # Product = Less fluorescent or Supercoiled DNA
 # Substrate = Concentration of Relaxed DNA or fluorescent
+initial_sigma = -0.02
+final_sigma = 0.0
 initial_product = 4.0
 initial_substrate = .75
+initial_substrates = [0.75, 1.50, 3.6, 5.4, 7.2]
 enzyme_concentration = 44.6
 K_M = 2.7
 k_cat = .0011
 v_max = k_cat * enzyme_concentration
-# Substrates and products
-# ----------------------------------
-ax = axs[0, 1]
-substrate, product = integrate_MM(vmax=v_max, KM=K_M, substrate0=initial_substrate, product0=initial_product,
-                                  frames=frames, dt=dt)
-ax.plot(time, substrate)
-# Sigma deduction
-# ----------------------------------
-ax = axs[1, 1]
-superhelical = rescale_product_to_sigma(substrate, initial_sigma, final_sigma)
-ax.plot(time, superhelical)
+for count, initial_substrate in enumerate(initial_substrates):
+    # Substrates and products
+    # ----------------------------------
+    ax = axs[0, 1]
+    substrate, product = integrate_MM(vmax=v_max, KM=K_M, substrate0=initial_substrate, product0=initial_product,
+                                      frames=frames, dt=dt)
+    ax.plot(time, substrate, lw=lw, color=colors[count])
 
-for ax in [axs[0,0], axs[0,1]]:
+    # Sigma deduction
+    # ----------------------------------
+    ax = axs[1, 1]
+    superhelical = rescale_product_to_sigma(substrate, initial_sigma, final_sigma)
+    ax.plot(time, superhelical, lw=lw, color=colors[count])
+
+for ax in [axs[0, 0], axs[0, 1]]:
     ax.set_xlabel('time (s)')
     ax.set_ylabel('Relaxed DNA (nM)')
     ax.grid(True)
 
-for ax in [axs[1,0], axs[1,1]]:
+for ax in [axs[1, 0], axs[1, 1]]:
     ax.set_xlabel('time (s)')
     ax.set_ylabel('Superhelical response')
     ax.grid(True)
--- TORCphysics/src/params.py
+++ TORCphysics/src/params.py
@@ -56,7 +56,7 @@ topo_t = -0.04  # thresholds
 # GYRASE
 gyra_w = 0.025  # width
 gyra_t = 0.01  # threshold
-# gyra_c = 0.25#.01 #concentration micromolarb 0.25 in meyer
+# gyra_c = 0.25#.01 #concentration micromolarb 0.25 in meyer - I'll use concentration in nM better
 # gyra_k = 0.001 #minus because it removes negative supercoils
 # I think it makes more sense to put the negative in the equation rather than in
 # the parameter
