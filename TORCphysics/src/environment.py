693fffb567222044b2b0d3a5de8dc0b39885533b
Victor <V.VelascoBerrelleza@leeds.ac.uk>
2023-08-15 18:39:53 +0100

    I am calibrating the topoisomerase activity according experimental assays

--- TORCphysics/src/environment.py
+++ TORCphysics/src/environment.py
@@ -1,27 +1,55 @@
 import pandas as pd
+from TORCphysics import binding_model as bm
 import sys
 from TORCphysics import params
 
 
 class Environment:
 
-    def __init__(self, e_type, name, site_list, concentration, k_on, k_off, size, site_type,
-                 binding_model, binding_oparams, effect_model, effect_oparams):  # , eff_model):
-        self.binding_oparams = None
-        self.effect_oparams = None
+    def __init__(self, e_type, name, site_list, concentration, size, site_type,
+                 binding_model_name, binding_oparams_file, effect_model_name, effect_oparams_file,
+                 unbinding_model_name, unbinding_oparams_file,
+                 binding_model=None, effect_model=None, unbinding_model=None):
+        #        self.binding_oparams = None
+        #        self.effect_oparams = None
+        self.binding_model = None
+        self.effect_model = None
+        self.unbinding_model = None
         self.enzyme_type = e_type
         self.name = name
         self.site_list = site_list  # It recognizes a list of sites, rather than a specific site
         self.site_type = site_type  # We need to remember the type
         self.concentration = concentration
-        self.k_on = k_on
-        self.k_off = k_off
         self.size = size
-        self.binding_model = binding_model
-        self.effect_model = effect_model
-        self.read_oparams(binding_oparams, effect_oparams)
-        self.binding_oparams_file = binding_oparams
-        self.effect_oparams_file = effect_oparams
+        self.binding_model_name = binding_model_name
+        self.binding_oparams_file = binding_oparams_file
+        self.effect_model_name = effect_model_name
+        self.effect_oparams_file = effect_oparams_file
+        self.unbinding_model_name = unbinding_model_name
+        self.unbinding_oparams_file = unbinding_oparams_file
+
+    def get_models(self, binding_model, effect_model, unbinding_model):
+
+        # Binding model
+        if issubclass(binding_model, bm.BindingModel):
+            self.binding_model = binding_model
+        if ((binding_model is None)
+                and (self.binding_model_name != 'none'
+                     or self.binding_model_name != 'None'
+                     or self.binding_model_name is not None)):
+            self.binding_model = bm.assign_binding_model(self.binding_model_name)
+
+        # Effect Model
+
+        # Unbinding Model
+        if issubclass(unbinding_model, bm.UnBindingModel):
+            self.unbinding_model = unbinding_model
+        if ((unbinding_model is None)
+                and (self.unbinding_model_name != 'none'
+                     or self.unbinding_model_name != 'None'
+                     or self.unbinding_model_name is not None)):
+            self.unbinding_model = bm.assign_unbinding_model(self.unbinding_model_name)
+
 
     def read_oparams(self, binding_oparams, effect_oparams):
 
@@ -40,8 +68,6 @@ class Environment:
             self.effect_oparams = pd.read_csv(effect_oparams).to_dict()
 
 
-
-
 class EnvironmentFactory:
     def __init__(self, filename, site_list):
         self.filename = filename
@@ -57,13 +83,14 @@ class EnvironmentFactory:
         for index, row in df.iterrows():
             new_environment = Environment(e_type=row['type'], name=row['name'],
                                           site_list=self.site_match(row['site_type']),
-                                          concentration=float(row['concentration']), k_on=float(row['k_on']),
-                                          k_off=float(row['k_off']), size=float(row['size']),
+                                          concentration=float(row['concentration']), size=float(row['size']),
                                           site_type=row['site_type'],
                                           binding_model=row['binding_model'],
                                           binding_oparams=row['binding_oparams'],
                                           effect_model=row['effect_model'],
-                                          effect_oparams=row['effect_oparams'])
+                                          effect_oparams=row['effect_oparams'],
+                                          unbinding_model=row['unbinding_model'],
+                                          unbinding_oparams=row['unbinding_oparams'])
             self.environment_list.append(new_environment)
 
     def site_match(self, label):
--- TORCphysics/tests/test_effects.py
+++ TORCphysics/tests/test_effects.py
@@ -6,7 +6,7 @@ class TestEffects(TestCase):
 
     def test_get_start_end_c(self):
         nbp = 5
-        my_site = Site(s_type='test_site', name='u1', start=1, end=nbp, k_min=0, k_max=0, s_model=None, oparams=None)
+        my_site = Site(s_type='test_site', name='u1', start=1, end=nbp, k_min=0, k_max=0, s_model_name=None, oparams=None)
         my_enzyme = Enzyme(e_type='test', name='YO', site=my_site, position=2, size=1, k_cat=0.0, k_off=0.0,
                            twist=0, superhelical=0)
         position_left, position_right = em.get_start_end_c(my_enzyme, my_enzyme, nbp)
@@ -15,7 +15,7 @@ class TestEffects(TestCase):
 
     def test_calculate_length(self):
         nbp = 2000
-        my_site = Site(s_type='test_site', name='u1', start=1, end=nbp, k_min=0, k_max=0, s_model=None, oparams=None)
+        my_site = Site(s_type='test_site', name='u1', start=1, end=nbp, k_min=0, k_max=0, s_model_name=None, oparams=None)
         enzyme1 = Enzyme(e_type='test', name='YO', site=my_site, position=200, size=50, k_cat=0.0, k_off=0.0,
                          twist=0, superhelical=0)
         enzyme2 = Enzyme(e_type='test', name='YO', site=my_site, position=1200, size=50, k_cat=0.0, k_off=0.0,
