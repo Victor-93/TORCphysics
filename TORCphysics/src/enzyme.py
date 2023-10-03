074920c252c1716ef7ba78bd233b9c2e035d280f
Victor <V.VelascoBerrelleza@leeds.ac.uk>
2023-10-03 14:49:35 +0100

    environment.py and site.py are documented!

--- TORCphysics/src/enzyme.py
+++ TORCphysics/src/enzyme.py
@@ -1,12 +1,15 @@
 import pandas as pd
 from TORCphysics import Site
+from TORCphysics import effect_model as em
+from TORCphysics import unbinding_model as ubm
 
 
 class Enzyme:
 
-    # TODO: Falta anadir el unbinding.
-    def __init__(self, e_type, name, site, position, size, effective_size, k_off, twist, superhelical,
-                 effect_model_name=None, effect_oparams_file=None, effect_model=None, effect_model_oparams=None):
+    def __init__(self, e_type, name, site, position, size, effective_size, twist, superhelical,
+                 effect_model_name=None, effect_oparams_file=None, effect_model=None, effect_model_oparams=None,
+                 unbinding_model_name=None, unbinding_oparams_file=None, unbinding_model=None,
+                 unbinding_model_oparams=None):
 
         # Assign parameters
         self.enzyme_type = e_type
@@ -15,12 +18,8 @@ class Enzyme:
         self.position = position
         self.size = size
         self.effective_size = effective_size
-        self.start = self.site.start
-        self.end = self.site.end
-        self.direction = self.site.get_direction()
         self.twist = twist
         self.superhelical = superhelical
-        self.k_off = k_off
 
         # Assign models
         self.effect_model_name = effect_model_name
@@ -28,8 +27,18 @@ class Enzyme:
         self.effect_model = effect_model
         self.effect_model_oparams = effect_model_oparams
 
+        self.unbinding_model_name = unbinding_model_name
+        self.unbinding_oparams_file = unbinding_oparams_file
+        self.unbinding_model = unbinding_model
+        self.unbinding_model_oparams = unbinding_model_oparams
+
         # Verify inputs
         self.check_inputs()
+
+        # If it passed the inputs, then calculate the values associated with the site
+        self.start = self.site.start
+        self.end = self.site.end
+        self.direction = self.site.get_direction()
 
         # Loads the binding, effect and unbinding models if given.
         self.get_models()
@@ -43,6 +52,7 @@ class Enzyme:
         if not isinstance(self.name, str) or self.name == '':
             raise ValueError('Error, enzymes must have a name')
         if not isinstance(self.site, Site):
+            print('Error for enzyme ', self.name)
             raise ValueError('Error, (bound) Enzymes must be linked to a Site')
         if not isinstance(self.position, float) and not isinstance(self.position, int):
             raise ValueError('Error, enzymes need a number for their position')
@@ -50,45 +60,80 @@ class Enzyme:
             raise ValueError('Error, enzymes need a number for size')
         if not isinstance(self.effective_size, float) and not isinstance(self.effective_size, int):
             raise ValueError('Error, enzymes need a number for effective size')
-        if not isinstance(self.start, float) and not isinstance(self.start, int):
-            raise ValueError('Error, enzymes need a number for start')
-        if not isinstance(self.end, float) and not isinstance(self.end, int):
-            raise ValueError('Error, enzymes need a number for start')
-        if not isinstance(self.direction, float) and not isinstance(self.direction, int):
-            raise ValueError('Error, enzymes need a number for direction')
+#        if not isinstance(self.start, float) and not isinstance(self.start, int):
+#            raise ValueError('Error, enzymes need a number for start')
+#        if not isinstance(self.end, float) and not isinstance(self.end, int):
+#            raise ValueError('Error, enzymes need a number for start')
+#        if not isinstance(self.direction, float) and not isinstance(self.direction, int):
+#            raise ValueError('Error, enzymes need a number for direction')
         if not isinstance(self.twist, float) and not isinstance(self.twist, int):
             raise ValueError('Error, enzymes need a number for twist')
         if not isinstance(self.superhelical, float) and not isinstance(self.superhelical, int):
             raise ValueError('Error, enzymes need a number for superhelical')
-        if not isinstance(self.k_off, float) and not isinstance(self.k_off, int):
-            raise ValueError('Error, enzymes need a number for k_off')
 
         if self.effective_size > self.size:
             print('Error, effective size effective_size cannot be larger than size')
             print('effective_size:', self.effective_size)
             print('size', self.size)
-            print('For environmental ', self.name)
+            print('For enzyme ', self.name)
             raise ValueError('Error: effective_size > size')
 
+        # Effect model
+        if self.effect_model_name == '' or self.effect_model_name == 'None' or self.effect_model_name == 'none':
+            self.effect_model_name = None
+        if self.effect_model == '' or self.effect_model == 'None' or self.effect_model == 'none':
+            self.effect_model = None
+        if self.effect_oparams_file == '' or self.effect_oparams_file == 'None' or self.effect_oparams_file == 'none':
+            self.effect_oparams_file = None
+        if (self.effect_model_oparams == '' or self.effect_model_oparams == 'None' or
+                self.effect_model_oparams == 'none'):
+            self.effect_model_oparams = None
 
-    def read_oparams(self, oparams):
-        """
-        Reads oparams that are related to the site_model
+        # Unbinding model
+        if (self.unbinding_model_name == '' or self.unbinding_model_name == 'None'
+                or self.unbinding_model_name == 'none'):
+            self.unbinding_model_name = None
+        if self.unbinding_model == '' or self.unbinding_model == 'None' or self.unbinding_model == 'none':
+            self.unbinding_model = None
+        if (self.unbinding_oparams_file == '' or self.unbinding_oparams_file == 'None'
+                or self.unbinding_oparams_file == 'none'):
+            self.unbinding_oparams_file = None
+        if (self.unbinding_model_oparams == '' or self.unbinding_model_oparams == 'None' or
+                self.unbinding_model_oparams == 'none'):
+            self.unbinding_model_oparams = None
+
+    def get_models(self):
+        """ Loads the Enzyme's effect and unbinding models (if given).
         """
 
-        if oparams is None or oparams == 'none':
-            self.effect_oparams = None
-        else:
-            self.effect_oparams = pd.read_csv(oparams).to_dict()
+        # Effect Model
+        self.effect_model, self.effect_model_name, self.effect_oparams_file, self.effect_model_oparams = (
+            em.get_effect_model(self.name, self.effect_model, self.effect_model_name,
+                                self.effect_oparams_file, self.effect_model_oparams))
+
+        # Unbinding Model
+        self.unbinding_model, self.unbinding_model_name, self.unbinding_oparams_file, self.unbinding_model_oparams = (
+            ubm.get_unbinding_model(self.name, self.unbinding_model, self.unbinding_model_name,
+                                    self.unbinding_oparams_file, self.unbinding_model_oparams))
+
+
 
 
 class EnzymeFactory:
 
-    def __init__(self, filename, site_list):
+    def __init__(self, filename=None, site_list=None):
         self.filename = filename
-        self.enzyme_list = []
+        if site_list is not None:  # In case site_list is given but is not a list
+            if not isinstance(site_list, list):
+                raise ValueError('Error in EnzymeFactory. site_list must be a list if given.')
         self.site_list = site_list
-        self.read_csv()
+        self.enzyme_list = []
+        if filename:
+            if site_list is None:  # In case site_list is given but is not a list
+                raise ValueError('Error in EnzymeFactory. filename provided but site_list is missing.')
+            if len(site_list) == 0:
+                raise ValueError('Error in EnzymeFactory. filename provided but empty site_list.')
+            self.read_csv()
 
     def get_enzyme_list(self):
         return self.enzyme_list
@@ -97,10 +142,14 @@ class EnzymeFactory:
     def read_csv(self):
         df = pd.read_csv(self.filename)
         for index, row in df.iterrows():
-            new_enzyme = Enzyme(e_type=row['type'], name=row['name'], site=self.site_match(row['site']),
-                                position=float(row['position']), size=float(row['size']), k_off=float(row['k_off']),
+            new_enzyme = Enzyme(e_type=row['type'], name=row['name'],
+                                site=self.site_match(row['site']), position=float(row['position']),
+                                size=float(row['size']), effective_size=float(row['effective_size']),
                                 twist=float(row['twist']), superhelical=float(row['superhelical']),
-                                effect_model=row['effect_model'], effect_oparams=row['effect_oparams'])
+                                effect_model_name=row['effect_model'],
+                                effect_oparams_file=row['effect_oparams'],
+                                unbinding_model_name=row['unbinding_model'],
+                                unbinding_oparams_file=row['unbinding_oparams'])
 
             self.enzyme_list.append(new_enzyme)
 
--- TORCphysics/tests/test_enzyme.py
+++ TORCphysics/tests/test_enzyme.py
@@ -1,8 +1,61 @@
 from unittest import TestCase
+from TORCphysics import Site, SiteFactory
 from TORCphysics import Enzyme, EnzymeFactory, SiteFactory
 
 
+# TODO: We still need to do the other effect tests and the unbinding tests
+
 class TestEnzyme(TestCase):
+
+    # Reads an Enzyme csv file, where the site does not exist in the site_list.
+    def test_Enzyme_bad_site(self):
+        site_gene1 = Site(site_type='gene', name='test_gene1', start=100, end=500, k_on=3.00)
+        site_gene2 = Site(site_type='gene', name='test_gene2', start=600, end=800, k_on=3.00)
+        site_gene3 = Site(site_type='gene', name='test_gene3', start=1200, end=1000, k_on=3.00)
+        site_tf = Site(site_type='TF', name='test_TF', start=1200, end=1000, k_on=3.00)
+        site_list = [site_gene1, site_gene2, site_gene3, site_tf]
+        enzyme_file = 'test_inputs/test_enzyme/enzyme_bad_site.csv'
+        with self.assertRaises(ValueError) as context:
+            EnzymeFactory(filename=enzyme_file, site_list=site_list)
+        self.assertEqual(str(context.exception), 'Error, (bound) Enzymes must be linked to a Site')
+
+    # Tests EnzymeFactory with the possible bad inputs
+    def test_bad_EnzymeFactory(self):
+        enzyme_file = 'test_inputs/test_enzyme/enzyme_effect.csv'
+
+        # Filename given but no site_list
+        with self.assertRaises(ValueError) as context:
+            EnzymeFactory(filename=enzyme_file)
+        self.assertEqual(str(context.exception), 'Error in EnzymeFactory. filename provided but site_list is missing.')
+
+        # site_list given but it is not a list
+        with self.assertRaises(ValueError) as context:
+            EnzymeFactory(site_list='Hola')
+        self.assertEqual(str(context.exception), 'Error in EnzymeFactory. site_list must be a list if given.')
+
+        # Filename given but site_list is an empty list
+        with self.assertRaises(ValueError) as context:
+            EnzymeFactory(filename=enzyme_file, site_list=[])
+        self.assertEqual(str(context.exception), 'Error in EnzymeFactory. filename provided but empty site_list.')
+
+    # Reads enzyme csv with different conditions for effect models.
+    def test_enzyme_effect_csv(self):
+        # FROM CSV
+        # Cases to test csv:
+        #  1. Name = None; no model.
+        #  2. Name + oparams=None; Effect model with default params.
+        #  3. Name + oparams; Model with params.
+        site_gene1 = Site(site_type='gene', name='test_gene1', start=100, end=500, k_on=3.00)
+        site_gene2 = Site(site_type='gene', name='test_gene2', start=600, end=800, k_on=3.00)
+        site_gene3 = Site(site_type='gene', name='test_gene3', start=1200, end=1000, k_on=3.00)
+        site_tf = Site(site_type='TF', name='test_TF', start=1200, end=1000, k_on=3.00)
+        site_list = [site_gene1, site_gene2, site_gene3, site_tf]
+        enzyme_file = 'test_inputs/test_enzyme/enzyme_effect.csv'
+        csv_enzyme = EnzymeFactory(filename=enzyme_file, site_list=site_list)
+        self.assertEqual(len(csv_enzyme.get_enzyme_list()), 3)  # All loaded correctly
+        self.assertEqual(csv_enzyme.enzyme_list[0].effect_model, None)  # Check specifics...
+        self.assertEqual(csv_enzyme.enzyme_list[1].effect_model.velocity, 30)
+        self.assertEqual(csv_enzyme.enzyme_list[2].effect_model.velocity, 20)
 
     # Checks it's not empty and that it loaded the origin correctly
     def test_EnzymeFactory(self):
