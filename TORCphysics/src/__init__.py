1cea20ae63a2251861fa2bc3b8b26259364bf591
Victor-93 <vic.myth93@gmail.com>
2025-11-25 15:57:04 +0000

    New custom gyrase model examples

--- TORCphysics/src/__init__.py
+++ TORCphysics/src/__init__.py
@@ -5,3 +5,6 @@ package_dir = os.path.dirname(__file__)
 
 # Set the path to enzyme_shapes relative to the package directory
 enzyme_shapes_dir = os.path.join(package_dir, 'enzyme_shapes')
+
+# Set the path to enzyme_shapes relative to the package directory
+model_params_dir = os.path.join(package_dir, 'model_params')
